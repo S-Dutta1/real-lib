3549e10623517b38b8119217c3e52aa7524b37c3
debjyoti0891 <debjyoti08911@gmail.com>
2017-01-05 05:09:49 +0800

    changed theme to nature for sphinx

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0,"/home/debjyoti/Dropbox/workspace/projects/quantum/index_files/real-lib/")
-html_theme = "classic"
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -89,7 +89,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
